5315b0ef618c3682a1afab8d57e16b559041c730
Marc <marc.troughton@gmail.com>
2019-01-09 22:54:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector){
+ return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
+
+function deepestChild() {
+  let result = document.getElementById("grand-node").querySelectorAll("div")
+  let test
+  
+  for (let i = 0; i < result.length-1; i++) {
+    test = result[i].querySelector("div")
+  }
+
+return test
+}
